567db83cce1b9deb385e1f92ff85e57695d82e89
user <user@w347459.local>
2017-10-02 18:36:01 -0400

    Initial Commit

---
+++ sc01-ShakeMeGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sc01-ShakeMeGame
+//
+//  Created by user on 10/2/17.
+//  Copyright © 2017 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sc01-ShakeMeGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sc01-ShakeMeGame
+//
+//  Created by user on 10/2/17.
+//  Copyright © 2017 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
